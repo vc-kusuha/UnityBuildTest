f32bf91cc0e5fce470ba4d2a8ffaca9cba9d9915
unknown <dj.kusuha+github@gmail.com>
2019-08-08 16:38:32 +0900

    Add build sample files

---
+++ UnityProject/Assets/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public sealed class Rotate : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _rotateEulers;
+    
+    private void Update()
+    {
+        transform.Rotate(_rotateEulers, Space.World);
+    }
+}
