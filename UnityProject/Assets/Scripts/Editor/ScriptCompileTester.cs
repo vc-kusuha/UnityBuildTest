39f253830e29ce6843ce939b39bf52a2ff0363ba
Yusuke KANNO <dj.kusuha+github@gmail.com>
2019-08-27 11:10:40 +0900

    compile error test

---
+++ UnityProject/Assets/Scripts/Editor/ScriptCompileTester.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Build.Player;
+using UnityEngine;
+
+public static class ScriptCompileTester
+{
+    [MenuItem("Tools/Compile Check/All", priority = 100)]
+    private static void CompileTestForAll()
+    {
+        CompileTestForDebugWindows();
+        CompileTestForReleaseWindows();
+    }
+
+    [MenuItem("Tools/Compile Check/Windows(Debug)")]
+    private static void CompileTestForDebugWindows()
+    {
+        CompileTest(BuildTarget.StandaloneWindows64, BuildTargetGroup.Standalone, true);
+    }
+
+    [MenuItem("Tools/Compile Check/Windows(Release)")]
+    private static void CompileTestForReleaseWindows()
+    {
+        CompileTest(BuildTarget.StandaloneWindows64, BuildTargetGroup.Standalone, false);
+    }
+
+    private static void CompileTest(BuildTarget buildTarget, BuildTargetGroup buildTargetGroup, bool isDevelopmentBuild)
+    {
+        const string tempBuildPath = "Temp/CompileTest";
+
+        var settings = new ScriptCompilationSettings
+        {
+            target = buildTarget,
+            group = buildTargetGroup,
+            options = isDevelopmentBuild ? ScriptCompilationOptions.DevelopmentBuild : ScriptCompilationOptions.None
+        };
+
+        var result = PlayerBuildInterface.CompilePlayerScripts(settings, tempBuildPath);
+
+        if (result.assemblies != null && result.assemblies.Any() && result.typeDB != null)
+        {
+            Debug.Log($"Compile Test Success! - BuildTarget: {settings.target}");
+        }
+
+        // NOTE: tempBuildPathにはコンパイル後のDLLが吐き出されている
+        if (Directory.Exists(tempBuildPath))
+        {
+            Directory.Delete(tempBuildPath, true);
+        }
+    }
+}
--- UnityProject/Assets/Scripts/Rotate.cs
+++ UnityProject/Assets/Scripts/Rotate.cs
@@ -1,10 +1,17 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public sealed class Rotate : MonoBehaviour
 {
-    [SerializeField]
-    private Vector3 _rotateEulers;
-    
+    [SerializeField] private Vector3 _rotateEulers;
+
+#if !DEBUG
+    private void Start()
+    {
+        死
+    }
+#endif
+
     private void Update()
     {
         transform.Rotate(_rotateEulers, Space.World);
